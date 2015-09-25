a5fb81d9a2b838096a440a212fbed707651854a7
raymondberg <dev@raymondberg.com>
2015-09-25 07:59:28 +0000

    I fixed it for Eric

--- first_run.py
+++ first_run.py
@@ -4,14 +4,7 @@ import itertools
 import scipy.misc
 import numpy
 
-extract_scores = lambda y:y["test_score"]
-
-def score_group(group):
-    return sum(map(extract_scores,group))/len(group)
-
-def findsubsets(S,m):
-        return set(itertools.combinations(S, m))
-
+extract_scores = lambda y:y.score
 class student:
     def __init__(self,score):
         self.score = score
@@ -19,6 +12,23 @@ class student:
         return "%d" % self.score
     def __repr__(self):
         return self.__str__()
+
+history_record = { "1": { "2": 2, "3": 3}}
+
+def history_score_group(group):
+    total_recurrences = 0
+    for i in range(0, len(group)):
+            for j in range(i+1,len(group)):
+                total_recurrences += history_record[str(i)][str(j)]
+    return total_recurrences
+
+def test_score_group(group):
+    return numpy.std(map(extract_scores,group))
+    # return sum(map(extract_scores,group))/len(group)
+
+
+def findsubsets(S,m):
+        return set(itertools.combinations(S, m))
 
 def students_in_groups(groups):
     students_included = 0
@@ -35,7 +45,7 @@ def groups_conflict(group_of_groups,new_group):
 
 def all_possible_class_combinations(total_group_choices, total_student_size, chosen_groups=[],start=0):
     if total_student_size == students_in_groups(chosen_groups):
-        print("Found one! %s " % str(chosen_groups))
+        # print("Found one! %s " % str(chosen_groups))
         return [chosen_groups]
 
     combinations = []
@@ -46,8 +56,8 @@ def all_possible_class_combinations(total_group_choices, total_student_size, cho
         combinations += all_possible_class_combinations(total_group_choices, total_student_size, chosen_groups + [group],index)
     return combinations
 
-total_class_size = 24
-students_per_group = 6
+total_class_size = 10
+students_per_group = 4
 
 master_class_list = []
 for i in range(0,total_class_size):
@@ -67,3 +77,21 @@ for class_possibility in all_possible:
     print("this is totally a possibility: %s \n\n" % str(class_possibility))
 
 print("There were a total of %d possibilities given a class size of %d and group size around %d" % (len(all_possible), len(master_class_list), students_per_group))
+
+class_summaries = {} 
+
+for class_index in range(0,len(all_possible)):
+    class_possibility = all_possible[class_index]
+    total_of_std = 0
+    for group in class_possibility:
+        total_of_std += test_score_group(group)
+        # total_of_history += history_score_group(group)
+    std_average = total_of_std / float(len(class_possibility))
+    class_summaries[class_index] = { "std_average": std_average}
+
+print(class_summaries)
+sorted_deviations = sorted(map(lambda k: class_summaries[k]['std_average'], class_summaries))
+print("Least diverse: %f" % sorted_deviations[0])
+print("Most diverse: %f" % sorted_deviations[-1])
+chosen_one = dict(("class",all_possible[key]) for key, value in class_summaries.iteritems() if value["std_average"] == sorted_deviations[-1])
+print("This is the best class for you: %s" % str(chosen_one))
